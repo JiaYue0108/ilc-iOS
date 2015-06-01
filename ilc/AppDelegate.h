babc4ed8932c5ac7d0f87ce81f06e651ee274d78
JiaYue <956019571@qq.com>
2015-06-01 11:04:46 +0800

    Initial Commit

---
+++ ilc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ilc
+//
+//  Created by 贾悦 on 15/6/1.
+//  Copyright (c) 2015年 贾悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ilc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ilc
+//
+//  Created by 贾悦 on 15/6/1.
+//  Copyright (c) 2015年 贾悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
